11a6f4db45148b562ee4452e9d414b75f5450ece
Rainer Brang <Rainer.Brang@winzki.de>
2020-11-25 17:03:17 +0100

    Found a nice dark mode color scheme.
     It's less ugly under windows when it comes to disabled menu entries.

--- hoerbert/main.cpp
+++ hoerbert/main.cpp
@@ -155,7 +155,7 @@ int main(int argc, char *argv[])
     if (!dir.exists())
     {
         if (!dir.mkpath(HOERBERT_TEMP_PATH))
-            qDebug() << "Failed making temp directory for ripping.";
+            qDebug() << "Failed creating temp directory for ripping.";
     }
 
     QFile ffmpeg_file(FFMPEG_PATH);
@@ -238,20 +238,29 @@ int main(int argc, char *argv[])
 
     if( darkMode == "true" ){
         QPalette palette;
-        // Now use a palette to switch to dark colors:
-        palette.setColor(QPalette::Window, QColor(53, 53, 53));
-        palette.setColor(QPalette::WindowText, Qt::white);
-        palette.setColor(QPalette::Base, QColor(25, 25, 25));
-        palette.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
-        palette.setColor(QPalette::ToolTipBase, QColor(53, 53, 53));
-        palette.setColor(QPalette::ToolTipText, Qt::white);
-        palette.setColor(QPalette::Text, Qt::white);
-        palette.setColor(QPalette::Button, QColor(53, 53, 53));
-        palette.setColor(QPalette::ButtonText, Qt::white);
-        palette.setColor(QPalette::BrightText, Qt::red);
-        palette.setColor(QPalette::Link, QColor(42, 130, 218));
-        palette.setColor(QPalette::Highlight, QColor(42, 130, 218));
-        palette.setColor(QPalette::HighlightedText, Qt::black);
+        // Now use a palette to switch to dark colors.
+        // I found this nice color scheme at https://github.com/Jorgen-VikingGod/Qt-Frameless-Window-DarkStyle/blob/master/DarkStyle.cpp
+        palette.setColor(QPalette::Window,QColor(53,53,53));
+        palette.setColor(QPalette::WindowText,Qt::white);
+        palette.setColor(QPalette::Disabled,QPalette::WindowText,QColor(127,127,127));
+        palette.setColor(QPalette::Base,QColor(42,42,42));
+        palette.setColor(QPalette::AlternateBase,QColor(66,66,66));
+        palette.setColor(QPalette::ToolTipBase,Qt::white);
+        palette.setColor(QPalette::ToolTipText,Qt::white);
+        palette.setColor(QPalette::Text,Qt::white);
+        palette.setColor(QPalette::Disabled,QPalette::Text,QColor(127,127,127));
+        palette.setColor(QPalette::Dark,QColor(35,35,35));
+        palette.setColor(QPalette::Shadow,QColor(20,20,20));
+        palette.setColor(QPalette::Button,QColor(53,53,53));
+        palette.setColor(QPalette::ButtonText,Qt::white);
+        palette.setColor(QPalette::Disabled,QPalette::ButtonText,QColor(127,127,127));
+        palette.setColor(QPalette::BrightText,Qt::red);
+        palette.setColor(QPalette::Link,QColor(42,130,218));
+        palette.setColor(QPalette::Highlight,QColor(42,130,218));
+        palette.setColor(QPalette::Disabled,QPalette::Highlight,QColor(80,80,80));
+        palette.setColor(QPalette::HighlightedText,Qt::white);
+        palette.setColor(QPalette::Disabled,QPalette::HighlightedText,QColor(127,127,127));
+
         QApplication::setPalette(palette);
 
         a.setStyle( "#ColorblindHint { background-color:#353535; }" );
