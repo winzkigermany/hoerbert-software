ddfbe1192d04530e92ea2d905c8ac739205311a7
Rainer Brang <Rainer.Brang@winzki.de>
2020-12-04 12:33:26 +0100

    Fixed path for macdeployqt
    It's very uncool to have this absolute path to macdeployqt. This needs a better way...

--- mac-deploy.sh
+++ mac-deploy.sh
@@ -2,5 +2,5 @@
 
 export PATH="$HOME/Qt/5.13.1/clang_64/bin:$PATH"
 
-macdeployqt ../Build/hoerbert.app -always-overwrite
+/Users/rainer/Qt/5.13.2/clang_64/bin/macdeployqt ../Build/hoerbert.app -always-overwrite
 ./macdeployqtfix.py ../Build/hoerbert.app ~/Qt/5.13.1/
